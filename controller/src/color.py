4937ee8816dde4829cbc0ed55b1feeeaac8baff2
Niels Hoffmann <niels@zentralmaschine.net>
2024-11-12 16:52:49 +0100

    Add a opacity function

--- controller/src/color.py
+++ controller/src/color.py
@@ -6,3 +6,7 @@ COLOR_WHITE = (1, 1, 1)
 COLOR_BLUE = (0, 127, 255)
 COLOR_RED = (255, 0, 0)
 COLOR_GREEN = (0, 255, 0)
+
+
+def opacity(color: tuple, opacity: float) -> tuple:
+    return tuple(int(color[i] * opacity) for i in range(3))
