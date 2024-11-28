3e1a5149d8dd82be7aed09392dd295c30d0b6b8b
Niels Hoffmann <niels@zentralmaschine.net>
2024-11-28 22:16:27 +0100

    Configure garbage collection in the board

--- board/src/main.py
+++ board/src/main.py
@@ -1,6 +1,12 @@
 from led_matrix import LedMatrix
 from server import TcpServer
+import gc
 
+print("GC threhshold to begin with: ", gc.threshold())
+gc.enable()
+
+# TODO make this configurable from the outside, so we don't have to flash the
+# board every time we want to change the size
 WIDTH = 8
 HEIGHT = 32
 
@@ -8,4 +14,9 @@ led_matrix = LedMatrix(WIDTH, HEIGHT)
 led_matrix.test()
 
 server = TcpServer()
-server.handle_requests(led_matrix.write)
+gc.collect()
+gc.threshold(gc.mem_free() // 4 + gc.mem_alloc())
+
+print("GC threhshold: ", gc.threshold())
+
+server.handle_requests(led_matrix.write_array)
