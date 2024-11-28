d5f5e92033a6c67cdfdbed0260eb71678f9248ea
Niels Hoffmann <niels@zentralmaschine.net>
2024-11-28 22:13:12 +0100

    Use numpy for pixel arrays

--- controller/src/surface_base.py
+++ controller/src/surface_base.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+import numpy as np
 
 import logging
 import color
@@ -28,14 +29,14 @@ class AbstractSurface(ABC):
         self.size = Size(dimension[0], dimension[1])
         self.position = Position(position[0], position[1])
 
-        self.pixels = [color.COLOR_BLACK] * self.size.area()
+        self.pixels = np.full((self.size.area(), 3), color.COLOR_BLACK)
 
     @abstractmethod
     def write(self):
         pass
 
     def fill(self, pixel_color):
-        self.pixels = [pixel_color] * len(self.pixels)
+        self.pixels = np.full_like(self.pixels, pixel_color)
 
     def draw_pixel(self, x, y, pixel_color):
         index = self.pixel_index(x, y)
@@ -44,5 +45,6 @@ class AbstractSurface(ABC):
 
     def pixel_index(self, x, y):
         if x < 0 or x >= self.size.width or y < 0 or y >= self.size.height:
+            # pixel not on the surface
             return None
         return y * self.size.width + x
