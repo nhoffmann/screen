83ddecbf016afeb89fed24f7c87c08fa6a6e168c
Niels Hoffmann <niels@zentralmaschine.net>
2024-11-28 22:17:59 +0100

    Switch accepted data format to struct

--- board/src/server.py
+++ board/src/server.py
@@ -3,29 +3,30 @@ import socket
 import select
 import json
 import struct
-import utime as time
+import time
 
-DEFAULT_PORT = 50_000
-DEFAULT_NUM_CONNECTIONS = 10
-DEFAULT_BUFFER_SIZE = 4096 #8192 # arbitrary multiple of 2, should probably be around the max size of all the boards
+_DEFAULT_PORT = const(50_000)
+_DEFAULT_NUM_CONNECTIONS = const(50)
+_DEFAULT_BUFFER_SIZE = const(
+    8192
+)  # arbitrary multiple of 2, should probably be around the max size of all the boards
 
-POLL_TIMEOUT = 1000
-MESSAGE_HEADER_LEN = 2
+_MESSAGE_HEADER_LEN = const(2)
+_HEADER_MARKER = const(">H")
 
-READ_ONLY = ( select.POLLIN |
-            select.POLLHUP |
-            select.POLLERR )
-READ_WRITE = READ_ONLY | select.POLLOUT
+_READ_ONLY = select.POLLIN | select.POLLHUP | select.POLLERR
+
 
 def device_ip():
     sta_if = network.WLAN(network.STA_IF)
-    return sta_if.ipconfig('addr4')[0]
+    return sta_if.ipconfig("addr4")[0]
+
 
 class Message:
     def __init__(self, poller, sock, address, buffer_size):
         self.poller = poller
         self.socket = sock
-        self.address = address 
+        self.address = address
         self._buffer_size = buffer_size
 
         self._receive_buffer = b""
@@ -42,85 +43,57 @@ class Message:
         if event_mask & select.POLLERR:
             print("An error occured")
 
-    def _read(self):
+    def read(self):
+        self._read_data_from_socket()
+
+        if not self._content_length:
+            self._process_protoheader()
+
+        if self._content_length and len(self._receive_buffer) >= self._content_length:
+            self._process_request()
+
+    def _read_data_from_socket(self):
         try:
             data = self.socket.recv(self._buffer_size)
         except:
-            pass 
+            pass
         else:
             if data:
                 self._receive_buffer += data
-            else: 
+                # print("Received data: ", len(self._receive_buffer))
+                # print("Free memory: ", gc.mem_free())
+                # print("Allocated memory: ", gc.mem_alloc())
+            else:
                 raise RuntimeError("Peer closed")
 
-    def read(self):
-        self._read()
-
-        if self._content_length is None:
-            self._process_protoheader()
-
-        if self._content_length is not None:
-            self.process_request()
-
     def _process_protoheader(self):
-        if len(self._receive_buffer) >= MESSAGE_HEADER_LEN:
-            self._content_length = struct.unpack(">H", self._receive_buffer[:MESSAGE_HEADER_LEN])[0]
-            self._receive_buffer = self._receive_buffer[MESSAGE_HEADER_LEN:]
+        if len(self._receive_buffer) >= _MESSAGE_HEADER_LEN:
+            self._content_length = struct.unpack(
+                _HEADER_MARKER, self._receive_buffer[:_MESSAGE_HEADER_LEN]
+            )[0]
+            self._receive_buffer = self._receive_buffer[_MESSAGE_HEADER_LEN:]
 
-    def process_request(self):
-        if not len(self._receive_buffer) >= self._content_length:
-            # print(f"Weird state {len(self._receive_buffer)} >= {self._content_length}")
-            ## TODO I don't understand this yet
-            # something like we have not read all the data so return early
-            return
-        # print(f"{len(self._receive_buffer)} >= {self._content_length}") 
-        data = self._receive_buffer[:self._content_length]
-        # print(f"data = {data}")
-        self._receive_buffer = self._receive_buffer[self._content_length:]
-        # print(f"self._receive_buffer = {self._receive_buffer}")
-        # self.request = self._hexarray_decode(data)
-        # print(f"self.request = {self.request}")
-        self.request = self._json_decode(data)
+    def _process_request(self):
+        # start = time.ticks_us()
+        num_pixels = self._content_length // 3
+        pixel_format = "BBB" * num_pixels
+        flat_pixels = struct.unpack(
+            pixel_format, self._receive_buffer[: self._content_length]
+        )
+        self.request = [
+            tuple(flat_pixels[i : i + 3]) for i in range(0, len(flat_pixels), 3)
+        ]
+        # print(f"Processing request takes: {time.ticks_us() - start} us")
+        self.close()
 
     def write(self):
         print("Socket wants to send data")
 
-    def _json_decode(self, json_bytes):
-        obj = json.loads(json_bytes)
-        return obj
-    
-    def _hexarray_decode(self, hexarray_bytes):
-        hex_string = hexarray_bytes.decode("utf-8")
-        step = 6
-        # return [self._hex2rgb(hex_string[i:i+step]) for i in range(0, len(hex_string), step)] 
-        t = time.ticks_us()
-        hex_arr = [hex_string[i:i+step] for i in range(0, len(hex_string), step)] 
-        color_array = []
-        for hexcode in hex_arr:
-            color_array.append(self._hex2rgb(hexcode))
-        delta = time.ticks_diff(time.ticks_us(), t)
-        print(f"Time to decode the hexarray: {delta/1000}ms")
-        return color_array
-
-    def _hex2rgb(self, hexcode):
-        t = time.ticks_us()
-
-        # result = tuple(int(hexcode[i:i+2], 16) for i in (0, 2, 4))
-        
-        result = ( int(hexcode[0:2], 16), int(hexcode[2:4], 16), int(hexcode[4:6], 16) )
-
-        delta = time.ticks_diff(time.ticks_us(), t)
-        print(f"Time to decode the hexstring: {delta/1000}ms")
-        return result
-        
     def close(self):
         try:
             self.poller.unregister(self.socket)
         except Exception as e:
-            print(
-                f"Error: poll.unregister() exception for "
-                f"{self.address}: {e!r}"
-            )
+            print(f"Error: poll.unregister() exception for " f"{self.address}: {e!r}")
 
         try:
             self.socket.close()
@@ -129,16 +102,24 @@ class Message:
         finally:
             # Delete reference to socket object for garbage collection
             self.socket = None
+            # Delete receive buffer for garbage collection
+            self._receive_buffer = None
 
 
 class TcpServer:
-    def __init__(self, ip_address = None, port = DEFAULT_PORT, num_connections = DEFAULT_NUM_CONNECTIONS, buffer_size = DEFAULT_BUFFER_SIZE):
+    def __init__(
+        self,
+        ip_address=None,
+        port=_DEFAULT_PORT,
+        num_connections=_DEFAULT_NUM_CONNECTIONS,
+        buffer_size=_DEFAULT_BUFFER_SIZE,
+    ):
         if ip_address:
             self._ip_address = ip_address
         else:
             self._ip_address = device_ip()
 
-        self._port = port 
+        self._port = port
         self._num_connections = num_connections
         self._buffer_size = buffer_size
         self._poller = select.poll()
@@ -148,7 +129,7 @@ class TcpServer:
         self._socket.listen(self._num_connections)
         self._socket.setblocking(False)
 
-        self._poller.register(self._socket, READ_ONLY)
+        self._poller.register(self._socket, _READ_ONLY)
         self._message_map = {}
 
         print(f"TCP server listening on: {(self._ip_address, self._port)}")
@@ -158,25 +139,34 @@ class TcpServer:
             events = self._poller.poll()
 
             for event_socket, event_mask in events:
+                event_socket_fileno = event_socket.fileno()
                 if event_socket is self._socket:
                     self._accept_connection()
                 else:
-                    message = self._message_map[event_socket.fileno()]
+                    message = self._message_map[event_socket_fileno]
                     try:
                         message.process_events(event_mask)
-                        if message.request is not None:
+                        if message.request:
+                            # We have a complete message to process. Send it to the callback
                             callback(message.request)
+                            del self._message_map[event_socket_fileno]
+                            # gc.collect()
+                            # pass
                     except Exception as e:
                         print(f"Error: Exception for {message.address}: {e}")
-                    finally:
-                        message.close()
+                        try:
+                            message.close()
+                        except:
+                            pass
+                        del self._message_map[event_socket_fileno]
 
     def _accept_connection(self):
         try:
             connection, address = self._socket.accept()
             connection.setblocking(False)
             message = Message(self._poller, connection, address, self._buffer_size)
-            self._poller.register(connection, READ_ONLY)
+            self._poller.register(connection, _READ_ONLY)
             self._message_map[connection.fileno()] = message
+            # print("Message map length: ", len(self._message_map))
         except Exception as e:
             print(f"Error during accepting the connection: {e}")
